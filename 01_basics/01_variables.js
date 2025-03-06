30c0360daa62e7ca056a7252f288e3a29bb86300
sakshichib <157032642+sakshichib@users.noreply.github.com>
2025-03-06 18:53:15 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 11642   //the const is a keyword and it cannot be changed the value will remain same 
+let accountEmail = "sakshi9674@gmail.com"
+var accountPassword = "12341234"
+accountCity = "LosAngelas" // there are possibility that 
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"user strict"; // treat all JS code as newer version
+
+// alert( 3 + 3)  // it will not execute because we are running it on node not browser
+
+console.log(3+3)  
+console.log("Sakshi")
+
+
+
+let name = "Sakshi"
+let age = 19
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => (standanlone value)
+// undefined => Value is not defined yet
+// symbol => for uniqueness
+
+
+// object
+
+console.log(typeof "Sakshi");
+console.log(typeof null);
+console.log(typeof undefined);
