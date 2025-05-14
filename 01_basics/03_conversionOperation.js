2bed03accd7b4f5da2b4afb04f0926a837ae8196
sakshichib <157032642+sakshichib@users.noreply.github.com>
2025-05-14 20:17:12 +0000

    Conversion Of  DataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let isNumber = Number(score);
+console.log(typeof isNumber);
+
+// "33" => 33
+// "33sakshi" => NaN
+// true => 1 ; false => 0
+
+let isLoggedIn = "sakshi"
+
+let booleanLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanLoggedIn)
+
+// 1 => true; 0 =>false
+// "" => false
+// "Sakshi" =>  true
+
+
+
+// We use datatype in capital only i.e Number
